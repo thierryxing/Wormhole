e89900bbf90f7985b622f4dbd540a594b87483ce
Thierry <thierry.xing@gmail.com>
2017-04-20 18:34:19 +0800

    add filter

--- Wormhole.py
+++ Wormhole.py
@@ -13,20 +13,24 @@ def sentry_msg():
     access_token = request.args.get('access_token')
     msg = request.get_json(silent=True)
     print(msg)
-    dingtalk_data = {
-        'msgtype': 'link',
-        'link': {
-            'text': msg['message'] + '\n' + msg['culprit'],
-            'title': msg['project_name'],
-            'picUrl': SENTRY_IMAGE,
-            'messageUrl': msg['url']
-        }
-    }
-    if msg['tags']['url'] is not None:
-        requests.post('%s?access_token=%s' % (DINGTALK_HOST, access_token),
-                      json=dingtalk_data)
+    if msg['event']['tags'] is not None:
+        tags = msg['event']['tags']
+        for tag in tags:
+            if tag[0] == 'url':
+                dingtalk_data = {
+                    'msgtype': 'link',
+                    'link': {
+                        'text': msg['message'] + '\n' + msg['culprit'],
+                        'title': msg['project_name'],
+                        'picUrl': SENTRY_IMAGE,
+                        'messageUrl': msg['url']
+                    }
+                }
+                requests.post('%s?access_token=%s' % (DINGTALK_HOST, access_token),
+                              json=dingtalk_data)
+                break
     return 'Success'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='127.0.0.1', debug=True)
